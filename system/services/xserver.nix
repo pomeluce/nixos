c62ca841cfcedd5b019df45c55733684aa9b0f0c
Tso <62343478+pomeluce@users.noreply.github.com>
2026-01-05 20:43:20 +0800

    feat(niri): update niri opacity

--- system/services/xserver.nix
+++ system/services/xserver.nix
@@ -6,8 +6,10 @@
 }:
 let
   bg = pkgs.fetchurl {
-    url = "https://www.desktophut.com/files/ymkspRzeH0-wallpaper.mp4";
-    hash = "sha256-WG8UI10NA4EAx63SU4Wb8x0r2TX7bOT5qLB1jKaSTKg=";
+    # url = "https://www.desktophut.com/files/ymkspRzeH0-wallpaper.mp4";
+    # hash = "sha256-WG8UI10NA4EAx63SU4Wb8x0r2TX7bOT5qLB1jKaSTKg=";
+    url = "https://www.desktophut.com/files/IGMGGy5nHoWdmM1_Sasuke%20Uchiha%20-%20Red%20&%20Black%20Live%20Wallpaper.mp4";
+    hash = "sha256-zoBifhQ+/U6ouG5/vESAALf5bC/4EnG8hdRMLbYt8kY=";
   };
   silent = pkgs.silent.override {
     theme = "default";
